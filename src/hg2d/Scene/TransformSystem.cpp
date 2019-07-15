4ddb6ba1d0d6cb006a07289c46346525c69e4d8e
Vlad2001MFS <kopytin.vladislav2001@yandex.ru>
2019-07-15 18:09:16 +0300

    add: TransformSystem

---
+++ src/hg2d/Scene/TransformSystem.cpp
@@ -0,0 +1,129 @@
+#include "SceneSystem.hpp"
+#include "../Core/Engine.hpp"
+#include "hd/IO/hdSerializerRW.hpp"
+#include "TransformSystem.hpp"
+
+namespace hg2d {
+
+TransformComponent::TransformComponent() : mPos(0, 0), mAbsolutePos(0, 0), mSize(0, 0) {
+    mParent = nullptr;
+    mAngle = 0.0f;
+    mIsDirty = true;
+}
+
+void TransformComponent::updateAbsolutePosition() {
+    if (mIsDirty) {
+        mIsDirty = false;
+        for (auto &child : mChildren) {
+            child->mIsDirty = true;
+        }
+        if (getParent()) {
+            float parentAngle = getParent()->getAngle();
+            glm::vec2 pos = glm::vec2(
+                getPosition().x*std::cos(parentAngle) - getPosition().y*std::sin(parentAngle),
+                getPosition().x*std::sin(parentAngle) + getPosition().y*std::cos(parentAngle)
+            );
+            mAbsolutePos = getParent()->getAbsolutePosition() + pos;
+        }
+        else {
+            mAbsolutePos = mPos;
+        }
+    }
+}
+
+void TransformComponent::translate(const glm::vec2 &offset) {
+    mPos += offset;
+    mIsDirty = true;
+}
+
+void TransformComponent::scale(const glm::vec2 &scale) {
+    mSize += scale;
+    mIsDirty = true;
+}
+
+void TransformComponent::rotate(float angle) {
+    mAngle += angle;
+    mIsDirty = true;
+}
+
+void TransformComponent::setParent(TransformComponent *parent) {
+    if (parent != mParent) {
+        if (parent) {
+            if (mParent) {
+                auto it = std::find(mParent->mChildren.begin(), mParent->mChildren.end(), this);
+                if (it != mParent->mChildren.end()) {
+                    *it = nullptr;
+                }
+            }
+            mParent = parent;
+            auto it = std::find(mParent->mChildren.begin(), mParent->mChildren.end(), nullptr);
+            if (it != mParent->mChildren.end()) {
+                *it = this;
+            }
+            else {
+                parent->mChildren.push_back(this);
+            }
+        }
+        else if (mParent) {
+            auto it = std::find(mParent->mChildren.begin(), mParent->mChildren.end(), this);
+            if (it != mParent->mChildren.end()) {
+                *it = nullptr;
+            }
+            mParent = nullptr;
+        }
+        mIsDirty = true;
+    }
+}
+
+void TransformComponent::setPosition(const glm::vec2 &pos) {
+    mPos = pos;
+    mIsDirty = true;
+}
+
+void TransformComponent::setSize(const glm::vec2 &size) {
+    mSize = size;
+    mIsDirty = true;
+}
+
+void TransformComponent::setAngle(float angle) {
+    mAngle = angle;
+    mIsDirty = true;
+}
+
+TransformComponent *TransformComponent::getParent() const {
+    return mParent;
+}
+
+const std::vector<TransformComponent*> &TransformComponent::getChildren() const {
+    return mChildren;
+}
+
+const glm::vec2 &TransformComponent::getAbsolutePosition() const {
+    return mAbsolutePos;
+}
+
+const glm::vec2 &TransformComponent::getPosition() const {
+    return mPos;
+}
+
+const glm::vec2 &TransformComponent::getSize() const {
+    return mSize;
+}
+
+float TransformComponent::getAngle() const {
+    return mAngle;
+}
+
+TransformSystem::TransformSystem(Engine& engine) : AECSSystem(engine) {
+}
+
+void TransformSystem::onFixedUpdate() {
+    const std::vector<TransformComponent*> &transforms = mEngine.getSceneSystem().getComponents<TransformComponent>();
+    for (auto &transform : transforms) {
+        if (transform) {
+            transform->updateAbsolutePosition();
+        }
+    }
+}
+
+}
---
+++ src/hg2d/Scene/TransformSystem.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include "SceneSystem.hpp"
+#include "3rd/include/glm/glm.hpp"
+
+namespace hg2d {
+
+class TransformComponent : public AECSComponent {
+public:
+    TransformComponent();
+
+    void updateAbsolutePosition();
+
+    void translate(const glm::vec2 &offset);
+    void scale(const glm::vec2 &scale);
+    void rotate(float angle);
+
+    void setParent(TransformComponent *parent);
+    void setPosition(const glm::vec2 &pos);
+    void setSize(const glm::vec2 &size);
+    void setAngle(float angle);
+
+    TransformComponent *getParent() const;
+    const std::vector<TransformComponent*> &getChildren() const;
+    const glm::vec2 &getAbsolutePosition() const;
+    const glm::vec2 &getPosition() const;
+    const glm::vec2 &getSize() const;
+    float getAngle() const;
+
+private:
+    std::vector<TransformComponent*> mChildren;
+    TransformComponent *mParent;
+
+    glm::vec2 mPos, mAbsolutePos;
+    glm::vec2 mSize;
+    float mAngle;
+    bool mIsDirty;
+};
+
+class TransformSystem : public AECSSystem {
+public:
+    TransformSystem(Engine &engine);
+
+    void onFixedUpdate();
+};
+
+}
